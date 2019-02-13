f3b5afb8cd13200197c508a906c8b51e5d6c5747
Marcel Schramm <marceloschr@googlemail.com>
2019-02-13 20:23:52 +0100

    Move TabSize comment in texview.go back to where it belongs.

--- textview.go
+++ textview.go
@@ -12,12 +12,12 @@ import (
 	runewidth "github.com/mattn/go-runewidth"
 )
 
-// TabSize is the number of spaces with which a tab character will be replaced.
 var (
 	openColorRegex  = regexp.MustCompile(`\[([a-zA-Z]*|#[0-9a-zA-Z]*)$`)
 	openRegionRegex = regexp.MustCompile(`\["[a-zA-Z0-9_,;: \-\.]*"?$`)
 	newLineRegex    = regexp.MustCompile(`\r?\n`)
 
+	// TabSize is the number of spaces with which a tab character will be replaced.
 	TabSize = 4
 )
 
