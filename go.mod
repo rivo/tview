01c93abb295e2d0737de4f6a6186c728da4a87ed
Trevor Slocum <trevor@rocketnine.space>
2020-06-09 15:27:57 -0700

    Add support for italic text

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/rivo/tview
 go 1.12
 
 require (
-	github.com/gdamore/tcell v1.3.0
+	github.com/gdamore/tcell v1.4.0
 	github.com/lucasb-eyer/go-colorful v1.0.3
 	github.com/mattn/go-runewidth v0.0.9
 	github.com/rivo/uniseg v0.1.0
-	golang.org/x/sys v0.0.0-20200817155316-9781c653f443 // indirect
-	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634 // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
--- util.go
+++ util.go
@@ -20,7 +20,7 @@ const (
 
 // Common regular expressions.
 var (
-	colorPattern     = regexp.MustCompile(`\[([a-zA-Z]+|#[0-9a-zA-Z]{6}|\-)?(:([a-zA-Z]+|#[0-9a-zA-Z]{6}|\-)?(:([lbdru]+|\-)?)?)?\]`)
+	colorPattern     = regexp.MustCompile(`\[([a-zA-Z]+|#[0-9a-zA-Z]{6}|\-)?(:([a-zA-Z]+|#[0-9a-zA-Z]{6}|\-)?(:([lbdiru]+|\-)?)?)?\]`)
 	regionPattern    = regexp.MustCompile(`\["([a-zA-Z0-9_,;: \-\.]*)"\]`)
 	escapePattern    = regexp.MustCompile(`\[([a-zA-Z0-9_,;: \-\."#]+)\[(\[*)\]`)
 	nonEscapePattern = regexp.MustCompile(`(\[[a-zA-Z0-9_,;: \-\."#]+\[*)\]`)
@@ -140,9 +140,10 @@ func overlayStyle(background tcell.Color, defaultStyle tcell.Style, fgColor, bgC
 	if attributes == "-" {
 		style = style.Bold(defAttr&tcell.AttrBold > 0)
 		style = style.Blink(defAttr&tcell.AttrBlink > 0)
+		style = style.Dim(defAttr&tcell.AttrDim > 0)
+		style = style.Italic(defAttr&tcell.AttrItalic > 0)
 		style = style.Reverse(defAttr&tcell.AttrReverse > 0)
 		style = style.Underline(defAttr&tcell.AttrUnderline > 0)
-		style = style.Dim(defAttr&tcell.AttrDim > 0)
 	} else if attributes != "" {
 		style = style.Normal()
 		for _, flag := range attributes {
@@ -153,6 +154,8 @@ func overlayStyle(background tcell.Color, defaultStyle tcell.Style, fgColor, bgC
 				style = style.Bold(true)
 			case 'd':
 				style = style.Dim(true)
+			case 'i':
+				style = style.Italic(true)
 			case 'r':
 				style = style.Reverse(true)
 			case 'u':
